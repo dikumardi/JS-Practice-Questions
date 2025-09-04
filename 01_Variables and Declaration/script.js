64c5bedfd6fac637e61810958721bceb6dde37f7
Diku Mardi <dikumardi047@gmail.com>
2025-09-04 20:24:09 +0530

    Add variables & declaration

---
+++ 01_Variables and Declaration/script.js
@@ -0,0 +1,23 @@
+/**1. Create a variable `name` and store your name in it. Print it.**/
+let name ="yourName";
+
+/**2. Create variables `age` and `city`. Print them in a sentence. **/
+
+let age = 21 ;
+let city = "yourCity"
+console.log(age);
+console.log(city);
+
+/**3. What happens if you declare a variable but don’t assign a value?
+ * Answer: It will give undefined **/
+
+/** 4. Change the value of a variable after declaring it. Print both values. **/
+// let num = 10 ;
+// let num = 20 ;
+// console.log(num); it throw an error  cannot redeclare same variable name with let
+
+/** 5. Use `let`, `const`, and `var` to declare variables. What differences do you notice? **/
+let a = 10 ;
+var b = 17 ;
+const c = 21;
+//let is used to in Es6 concept can  reassinn but cannot redeclare and var is you can redeclare and reassing same variable name and it will give no error const is for assign constant to the value no change wanted to be done
