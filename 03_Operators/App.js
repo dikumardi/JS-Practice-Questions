3bedd87fc881a383bd6b7166d50f24c558a16f6c
Diku Mardi <dikumardi047@gmail.com>
2025-09-06 20:15:13 +0530

    Js Operators Practice question

---
+++ 03_Operators/App.js
@@ -0,0 +1,30 @@
+/** ## 3. Operators
+
+### Easy
+1. Multiply two numbers and print the result.
+2. Find remainder of 29 ÷ 6 using `%`.
+
+### Medium
+3. Compare `7 == "7"` and `7 === "7"`.
+4. Use `&&` and `||` in an example.
+
+### Hard
+5. What is the result of `!true && true`?
+6. Predict output: `console.log(2 + 3 * 4)`. **/
+
+console.log(10 * 2); //20
+
+console.log(29%6); //5
+
+console.log(7 == "7"); // true
+
+console.log(7 === "7"); //false
+
+console.log(!true && true); //false
+console.log(2 + 3 * 4); // 14
+
+
+
+
+
+
