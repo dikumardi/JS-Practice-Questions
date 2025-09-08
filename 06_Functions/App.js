77fe4ef50a7573d562e5ebae7fd1402907790c9c
Diku Mardi <dikumardi047@gmail.com>
2025-09-08 20:33:23 +0530

    update function practice question

---
+++ 06_Functions/App.js
@@ -0,0 +1,25 @@
+/** ### Easy
+1. Write a function that returns the square of a number.
+2. Write a function to greet a user.
+
+### Medium
+3. Write a function that checks if a number is prime.
+4. Write a function that reverses a string.
+
+### Hard
+5. Write a function that finds the largest number in an array.
+6. Write a recursive function to find factorial of a number.
+ **/
+
+function square(val) {
+    return val * val ;
+}
+console.log(square(17));
+
+function greet() {
+    console.log("Hello User");
+}
+
+greet();
+
+
