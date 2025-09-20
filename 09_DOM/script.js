6ed31da36afd2054fcc32ae95c7ac062347563ab
Diku Mardi <dikumardi047@gmail.com>
2025-09-20 21:14:09 +0530

    update

--- 09_DOM/script.js
+++ 09_DOM/script.js
@@ -5,17 +5,30 @@ Change the background color of the whole page to "lightblue".
 Create a button <button>Click me</button> and when clicked, it changes the text inside a <h1> tag. **/
 
 //Question 1 .
-let p = document.querySelector("#myText");
-p.textContent = "Hello JavaScript!"
+// let p = document.querySelector("#myText");
+// p.textContent = "Hello JavaScript!"
 
 //Question 2 .
-document.body.style.background = "lightblue"
+// document.body.style.background = "lightblue"
 
 //Question 3 .
-   let h1 = document.querySelector("h1");
-    let btn= document.querySelector("button")
+  //  let h1 = document.querySelector("h1");
+  //   let btn= document.querySelector("button")
     
-        btn.addEventListener("click", function () {
-          h1.textContent = "h1 text changed"
+  //       btn.addEventListener("click", function () {
+  //         h1.textContent = "h1 text changed"
             
+  //       });
+
+
+        //Question 4 . Create an input and a button. Show "Hello, [name]!" in <p> when button clicked.
+
+        let inp= document.querySelector("input");
+        let btn= document.querySelector("button");
+        let p= document.querySelector("p");
+        btn.addEventListener("click", function () {
+        console.log( inp.value);
+        
+         p.textContent = `Hello ${inp.value} !`
         });
+        
