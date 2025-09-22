dcd1bf9e13627a33c46fe05c4daa16625f2d1d86
Diku Mardi <dikumardi047@gmail.com>
2025-09-22 21:52:07 +0530

    update operator

---
+++ 03_Operators/pq.js
@@ -0,0 +1,8 @@
+/**1. Write a program to implement a simple calculator using only operators.
+2. Predict output: `console.log(1 < 2 < 3);` and `console.log(3 > 2 > 1);`.
+3. Use bitwise operators to check if a number is odd or even.
+4. Implement XOR using only `!`, `&&`, and `||`.
+**/
+
+console.log(1 < 2 < 3); //true
+console.log(3 > 2 > 1) //false
